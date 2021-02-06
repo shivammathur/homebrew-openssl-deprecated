89123b73a085e607e0d024be6f4cc069f65bee13
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-06 10:15:33 +0000

    meson@1.0: update 0.56.2 bottle.

--- Formula/meson@1.0.rb
+++ Formula/meson@1.0.rb
@@ -11,6 +11,8 @@ class MesonAT10 < Formula
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/openssl-deprecated"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "48bdf17986a783a0b6cdf82788623b104257c9345544f94feacf1f50a73287ac"
+    sha256 cellar: :any_skip_relocation, catalina:      "52e353f346277abbe36cbb048491827cbced318e6caa8c262ef2f4af35e506f5"
   end
 
   depends_on "shivammathur/openssl-deprecated/ninja@1.0"
